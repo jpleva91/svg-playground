af2e0157d81dd5c082d77b3003b8db8e53d852ff
jared-finthrive <jpleva91@gmail.com>
2023-03-01 12:59:18 -0500

    add web

---
+++ web/js/main.js
@@ -0,0 +1,42 @@
+const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+circle.setAttribute("cx", "50");
+circle.setAttribute("cy", "50");
+circle.setAttribute("r", "20");
+circle.setAttribute("fill", "blue");
+
+svg.appendChild(circle);
+
+document.body.appendChild(svg);
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "../img/drawing.svg");
+xhr.onload = function() {
+  const svgString = xhr.responseText;
+  const range = document.createRange();
+  range.selectNode(document.body);
+  const fragment = range.createContextualFragment(svgString);
+  const svg = fragment.querySelector("svg");
+  document.body.appendChild(svg);
+  const signIn = document.querySelector('[data-click-handler="sign-in-click-handler"]');
+
+  signIn.addEventListener("mouseover", function(event) {
+    signIn.style.fill = "red";
+  });
+  signIn.addEventListener("mouseout", function(event) {
+    signIn.style.fill = "black";
+  });
+
+  signIn.addEventListener("click", function(event) {
+    alert("You have been signed in!");
+  });
+
+  const star = document.querySelector('[data-click-handler="star-click-handler"]');
+  star.addEventListener("mouseover", function(event) {
+    star.style.fill = "yellow";
+  });
+  star.addEventListener("mouseout", function(event) {
+    star.style.fill = "black";
+    });
+  };
+xhr.send();
